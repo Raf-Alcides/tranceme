509e26f8d5bb386eb4ce5bffa42f3f883d6926be
Rafael Alcides <devrafalcides@gmail.com>
2023-04-21 20:24:07 -0300

    Criacao de class contendo cores do projeto

---
+++ lib/config/custom_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class CustomColors {
+  static Color primaryColor = const Color(0xff866d6d);
+  static Color secondColor = const Color(0xffffeee5);
+  static Color textColor = const Color(0xfff97115);
+   static Color cardColor = const Color(0xfff9f0f0);
+}

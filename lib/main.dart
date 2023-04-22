24f0b8ca296ac248e1728fceb686357eb0fa3d2d
Rafael da Silva Alcides <119860224+Raf-Alcides@users.noreply.github.com>
2023-04-22 15:45:35 -0300

    Atualização de imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 import 'package:tranceme/config/custom_colors.dart';
 import 'package:tranceme/splash_screen.dart';
 

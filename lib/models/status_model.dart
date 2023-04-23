da887114603014d4a75da4212d36c23d26954a09
Rafael da Silva Alcides <119860224+Raf-Alcides@users.noreply.github.com>
2023-04-23 12:43:00 -0300

    Exclusao de import desnecessarios

--- lib/models/status_model.dart
+++ lib/models/status_model.dart
@@ -1,8 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'dart:ffi';
-
-import 'package:flutter/material.dart';
-
 class StatusModel {
   String name;
   double preco;

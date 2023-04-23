0c02a5713f05f3ac0d37f4e593641576de4fc8cb
Rafael da Silva Alcides <119860224+Raf-Alcides@users.noreply.github.com>
2023-04-23 12:27:31 -0300

    Criacao da tela base do Projeto

--- lib/pages/base_page.dart
+++ lib/pages/base_page.dart
@@ -1,14 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:tranceme/config/custom_colors.dart';
+import 'package:tranceme/pages/home_tab.dart';
 
-class BasePage extends StatelessWidget {
+class BasePage extends StatefulWidget {
 
   const BasePage({ Key? key }) : super(key: key);
+
+  @override
+  State<BasePage> createState() => _BasePageState();
+}
+
+class _BasePageState extends State<BasePage> {
+
+  final pageController = PageController();
+  int currentIndex = 1;
 
    @override
    Widget build(BuildContext context) {
        return Scaffold(
-           appBar: AppBar(title: const Text(''),),
-           body: Container(),
+        backgroundColor: const Color(0xffe5e5e5),
+           body: PageView(
+            physics: const NeverScrollableScrollPhysics(),
+            controller: pageController,
+            children: [
+              Container(color: Colors.yellow,),
+              const HomeTab(),
+              Container(color: Colors.red,),
+            ],
+           ),
+           bottomNavigationBar: BottomNavigationBar(
+            onTap: (index) {
+              setState(() {
+                currentIndex = index;
+                pageController.jumpToPage(index);
+              });
+            },
+            currentIndex: currentIndex,
+            backgroundColor: Colors.white,
+            selectedItemColor: CustomColors.primaryColor,
+            unselectedItemColor: Colors.brown[200],
+            
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.chat),
+                label: 'Chat',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Inicio',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'Perfil',
+              ),
+            ]          ),
        );
   }
 }

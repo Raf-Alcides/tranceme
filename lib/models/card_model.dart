3792d8fc31429e53a495348696b52b84706599dc
Rafael da Silva Alcides <119860224+Raf-Alcides@users.noreply.github.com>
2023-04-23 12:42:03 -0300

    Criacao de modelos de dados

---
+++ lib/models/card_model.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+class CardModel {
+  String name;
+  String img;
+  String desciption;
+  double preco;
+  
+  CardModel({
+    required this.name,
+    required this.img,
+    required this.desciption,
+    required this.preco,
+  });
+}
---
+++ lib/models/status_model.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+class StatusModel {
+  String name;
+  double preco;
+  String img; 
+  
+  StatusModel({
+    required this.name,
+    required this.preco,
+    required this.img,
+  });
+}

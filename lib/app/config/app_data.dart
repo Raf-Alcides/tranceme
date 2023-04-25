c502ddd96627289b94512b9f5fccb8e21a706cb1
Rafael da Silva Alcides <119860224+Raf-Alcides@users.noreply.github.com>
2023-04-25 15:16:22 -0300

    Dados mocados do app

---
+++ lib/app/config/app_data.dart
@@ -0,0 +1,95 @@
+import 'package:tranceme/app/models/card_model.dart';
+import 'package:tranceme/app/models/status_model.dart';
+
+StatusModel s1 = StatusModel(
+  name: 'Dreads',
+  preco: 2,
+  img: 'assets/images/capaLoja.png',
+);
+
+StatusModel s2 = StatusModel(
+  name: 'Dreads',
+  preco: 2,
+  img: 'assets/images/capaLoja.png',
+);
+
+StatusModel s3 = StatusModel(
+  name: 'Dreads',
+  preco: 2,
+  img: 'assets/images/capaLoja.png',
+);
+
+StatusModel s4 = StatusModel(
+  name: 'Dreads',
+  preco: 2,
+  img: 'assets/images/capaLoja.png',
+);
+
+StatusModel s5 = StatusModel(
+  name: 'Dreads',
+  preco: 2,
+  img: 'assets/images/capaLoja.png',
+);
+
+List<StatusModel> status =  [
+  s1, s2, s3, s4, s5
+];
+
+CardModel card1 = CardModel(
+  name: 'Ray Penteados', 
+  img: 'assets/images/capaLoja.png', 
+  desciption: 'A melhor Trancista da cidade', 
+  preco: 29.90,
+);
+
+CardModel card2 = CardModel(
+  name: 'Eloa Trancas', 
+  img: 'assets/images/capaLoja.png', 
+  desciption: 'A melhor Trancista da cidade', 
+  preco: 29.90,
+);
+
+CardModel card3 = CardModel(
+  name: 'Afrodencia', 
+  img: 'assets/images/capaLoja.png', 
+  desciption: 'A melhor Trancista da cidade', 
+  preco: 29.90,
+);
+
+CardModel card4 = CardModel(
+  name: 'Preta', 
+  img: 'assets/images/capaLoja.png', 
+  desciption: 'A melhor Trancista da cidade', 
+  preco: 29.90,
+);
+
+CardModel card5 = CardModel(
+  name: 'ElaPode', 
+  img: 'assets/images/capaLoja.png', 
+  desciption: 'A melhor Trancista da cidade', 
+  preco: 29.90,
+);
+
+CardModel card6 = CardModel(
+  name: 'Studio Camila Alves Trancista', 
+  img: 'assets/images/capaLoja.png', 
+  desciption: 'A melhor Trancista da cidade', 
+  preco: 29.90,
+);
+
+CardModel card7 = CardModel(
+  name: 'Tatah Braids', 
+  img: 'assets/images/capaLoja.png', 
+  desciption: 'A melhor Trancista da cidade', 
+  preco: 29.90,
+);
+
+List<CardModel> cards = [
+  card1,
+  card2,
+  card3,
+  card4, 
+  card5,
+  card6,
+  card7
+];
